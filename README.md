0a03feda8aff5eb3a1dda23e524da46ddc952373
daniel garcia <coolxeo@gmail.com>
2013-06-09 18:45:29 +0200

    Basic structure with Boostrap 3 dashboard

--- README.md
+++ README.md
@@ -1,4 +1,14 @@
-linepos
+LinePOS
 =======
 
 Point Of Service made easy
+
+
+Tech Used
+===================
+
+Boostrap3 Dashboard
+http://bootply.com/61582
+
+Starter for Require and Backbone
+https://github.com/NETTUTS/RequireJS-and-Backbone-Starter
---
+++ app/scripts/main.js
@@ -0,0 +1,12 @@
+requirejs.config({
+	paths: {
+		'jquery': 'vendor/jquery/jquery',
+		'underscore': 'vendor/underscore-amd/underscore',
+		'backbone': 'vendor/backbone-amd/backbone',
+	}
+});
+
+requirejs(['views/app'], 
+	function(AppView) {
+		new AppView;
+});
